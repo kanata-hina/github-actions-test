81f29530d305df24111fe8cb24575fd7882e0181
小林 雅幸 <kobayashi.masayuki@nifty.co.jp>
2023-02-27 15:37:23 +0900

    cargo tomlとrustのテストの追加

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,13 @@
+pub fn add(x: i32, y: i32) -> i32{
+    x + y
+}
+
 #[cfg(test)]
 mod tests{
     #[test]
+        
     fn it_works(){
+        use crate::add;
         assert_eq!(add(1, 2), 3)
     }
-}
-
-fn add(x: i32, y: i32) -> i32{
-    x + y;
 }
